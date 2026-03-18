53dce53874779317e0030771c37a1940507d5953
MOUAD LAHBIB ARBAOUI <mouadlahbibarbaoui08@gmail.com>
2026-03-18 01:21:58 +0000

    SEARCH ENGINE

--- search/search_engine.py
+++ search/search_engine.py
@@ -2,38 +2,51 @@ from indexer.inverted_index import inverted_index
 from crawler.crawler import pages_data
 import string
 
-search_engine_docs_words_mentions = {}
 
-search = input("Qu'est ce que vous cherchez ... ")
+def search(query):
+    search_engine_docs_words_mentions = {}
 
-clean_search = search.lower()
-clean_search = clean_search.translate(str.maketrans('', '', string.punctuation))
-query_words = clean_search.split()
+    clean_query = query.lower()
+    clean_query = clean_query.translate(str.maketrans('', '', string.punctuation))
+    query_words = clean_query.split()
 
-for word in query_words:
-    if word in inverted_index:
-        for url in inverted_index[word]:
-            if url not in search_engine_docs_words_mentions:
-                search_engine_docs_words_mentions[url] = 0
+    for word in query_words:
+        if word in inverted_index:
+            for url in inverted_index[word]:
+                if url not in search_engine_docs_words_mentions:
+                    search_engine_docs_words_mentions[url] = 0
 
-            search_engine_docs_words_mentions[url] += 1
+                search_engine_docs_words_mentions[url] += 1
 
-sorted_results = sorted(
-    search_engine_docs_words_mentions.items(),
-    key=lambda item: item[1],
-    reverse=True
-)
+    sorted_results = sorted(
+        search_engine_docs_words_mentions.items(),
+        key = lambda item: item[1],
+        reverse = True
+    )
+    
+    results = []
 
-print("\nResults:\n")
-
-if len(sorted_results) == 0:
-    print("No results found.")
-else:
     for url, score in sorted_results:
-        title = pages_data[url]["title"]
-        print("-------------------------------------")
-        print("Title :", title)
-        print("URL   :", url)
-        print("Score :", score)
+        result = {
+            "title": pages_data[url]["title"],
+            "url": url,
+            "score": score
+        }
+        results.append(result)
 
-print("-------------------------------------")
+    return results
+
+
+if __name__ == "__main__":
+    user_query = input("Qu'est ce que vous cherchez ... ")
+    results = search(user_query)
+
+    if len(results) == 0:
+        print("No results found.")
+    else:
+        for result in results:
+            print("-------------------------------------")
+            print("Title :", result["title"])
+            print("URL   :", result["url"])
+            print("Score :", result["score"])
+        print("-------------------------------------")
